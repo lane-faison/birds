9bb285ce6108c5e55f0ca70d20e93a818de23821
lanefaison2 <faisonusmc@gmail.com>
2017-03-11 19:44:47 -0700

    More CSS, HTML, JS added

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -3,10 +3,10 @@ $(document).ready( () => {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
       var date = data[i].date.slice(0,10)
-      $('.bird-list').append(`<div class='bird-list-item'><h3>${data[i].name}</h3></div>`)
-      $('.location-list').append(`<div class='bird-list-item'><h3>${data[i].location}</h3></div>`)
-      $('.rating-list').append(`<div class='bird-list-item'><h3>${data[i].rating}</h3></div>`)
-      $('.date-list').append(`<div class='bird-list-item'><h3>${date}</h3></div>`)
+      $('.bird-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].name}</h3></div>`)
+      $('.location-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].location}</h3></div>`)
+      $('.date-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${date}</h3></div>`)
+      $('.rating-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].rating}</h3></div>`)
     }
   })
 })

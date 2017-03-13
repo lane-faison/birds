effc07946805874c51a108f5c4c8ccc50613f1b5
lanefaison2 <faisonusmc@gmail.com>
2017-03-12 19:16:24 -0600

    changed to table format, delete works

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -4,23 +4,27 @@ $(document).ready( () => {
     for (var i = 0; i < data.length; i++) {
       if (data[i].notes) {
         var date = data[i].date.slice(0,10)
-        $('.bird-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].name}</h3><a tabindex="0" class="show-field-notes btn btn-lg btn-info" role="button" data-toggle="popover" data-trigger="focus" title="Field Notes" data-content="${data[i].notes}"><span class="glyphicon glyphicon-leaf" aria-hidden="true"></span></a></div>`)
-
-        $('.location-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].area}</h3></div>`)
-
-        $('.date-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${date}</h3></div>`)
-
-        $('.rating-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].rating}</h3></div>`)
+        $('.bird-list').append(
+          `<tr class='bird-row' id=${data[i].id}>
+          <td class='td-bird-name'><h3>${data[i].name}</h3></td>
+          <td class='td-field-notes'><a tabindex="0" class="show-field-notes btn btn-lg btn-info" role="button" data-toggle="popover" data-trigger="focus" title="Field Notes" data-content="${data[i].notes}"><span class="glyphicon glyphicon-leaf" aria-hidden="true"></span></a></td>
+          <td><h3>${data[i].area}</h3></td>
+          <td class='td-rating'><h3>${data[i].rating}</h3></td>
+          <td class='date-options'><h3>${date}</h3><button type="button" class="delete-btn"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
+          <a href="edit.html?id=${data[i].id}"><button type="button" class="edit-btn"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></a></td>
+          </tr>`)
       }
       else {
         var date = data[i].date.slice(0,10)
-        $('.bird-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].name}</h3></div>`)
-
-        $('.location-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].area}</h3></div>`)
-
-        $('.date-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${date}</h3></div>`)
-
-        $('.rating-list').append(`<div class='bird-list-item' id=${data[i].id}><h3>${data[i].rating}</h3></div>`)
+        $('.bird-list').append(
+          `<tr class='bird-row' id=${data[i].id}>
+          <td><h3>${data[i].name}</h3></td>
+          <td></td>
+          <td><h3>${data[i].area}</h3></td>
+          <td><h3>${data[i].rating}</h3></td>
+          <td class='date-options'><h3>${date}</h3><button type="button" class="delete-btn"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
+          <a href="edit.html?id=${data[i].id}"><button type="button" class="edit-btn"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></a></td>
+          </tr>`)
       }
     }
   })
@@ -36,8 +40,23 @@ $(document).on('mouseleave','.link-bird-notes', function() {
   $(this).closest('div').removeClass('notes-frame')
 })
 
-
 $(document).on('click','.show-field-notes', function () {
-
   $(this).popover('show')
+})
+
+$(document).on('click','.delete-btn', function () {
+  var birdID = $(this).closest('tr').attr('id')
+  console.log(birdID);
+  $.ajax({
+    url: `/birds/bird/${birdID}`,
+    type: 'DELETE',
+    success: function (result) {
+      $(`#${birdID}`).remove()
+      console.log('BIRD DELETED');
+    },
+    error: function (result) {
+      console.log('Something went wrong when trying to delete this bird.')
+    }
+  })
+
 })
